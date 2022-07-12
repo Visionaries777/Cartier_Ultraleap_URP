002ea9aeeebdad23a9878ddff234110064a2c988
oscarTam <77429127+oscartsz@users.noreply.github.com>
2022-07-12 17:12:18 +0800

    add circular list movement

--- Assets/opt4/Scripts/CircularList.cs
+++ Assets/opt4/Scripts/CircularList.cs
@@ -7,14 +7,17 @@ public class CircularList : MonoBehaviour
 {
     public Opt4Display main;
     public GameObject font;
-    public float grid,pop;
+    public float grid,pop,speed;
     public int last,current;
-    public GameObject[] timeline;
+    public Color selectedColor,normalColor;
+    private GameObject[] timeline;
+    private Vector3[] targetPos;
 
 
     void Awake()
     {
         timeline = new GameObject[main.options.Length];
+        targetPos = new Vector3[timeline.Length];
     }
 
     private void Start()
@@ -24,6 +27,8 @@ public class CircularList : MonoBehaviour
             var o = Instantiate(font, this.transform.position, Quaternion.identity, this.transform);
             timeline[i] = o;
             timeline[i].GetComponent<TextMesh>().text = main.options[i].year;
+            timeline[i].transform.localPosition = new Vector3(pop * (i - current), grid * (i - current), pop * (i - current));
+            timeline[i].SetActive(true);
         }
     }
 
@@ -36,12 +41,14 @@ public class CircularList : MonoBehaviour
         {
             for (int i = current; i < timeline.Length; i++)
             {
-                timeline[i].transform.localPosition = new Vector3(pop * (i - current), grid * (i - current), pop * (i - current));
+                //timeline[i].transform.localPosition = new Vector3(pop * (i - current), grid * (i - current), pop * (i - current));
+                targetPos[i] = new Vector3(pop * (i - current), grid * (i - current), pop * (i - current));
                 timeline[i].SetActive(true);
             }
-            for (int i = 1; i <= (current - 0); i++)
+            for (int i = 1; i <= current; i++)
             {
-                timeline[current - i].transform.localPosition = new Vector3(pop * i, -grid * i, pop * i);
+                //timeline[current - i].transform.localPosition = new Vector3(pop * i, -grid * i, pop * i);
+                targetPos[current - i] = new Vector3(pop * i, -grid * i, pop * i);
                 timeline[i].SetActive(true);
             }
             if (current > 3)
@@ -58,7 +65,24 @@ public class CircularList : MonoBehaviour
                     timeline[i].SetActive(false);
                 }
             }
+            timeline[current].GetComponent<TextMesh>().color = selectedColor;
+            if(last!=-1)
+                timeline[last].GetComponent<TextMesh>().color = normalColor;
             last = current;
         }
+        Move();
     }
+
+    void Move()
+    {
+        for(int i = 0; i < timeline.Length; i++)
+        {
+            if (Vector3.Distance(timeline[i].transform.localPosition, targetPos[i]) >= 0.01)
+            {
+                timeline[i].transform.localPosition = Vector3.Lerp(timeline[i].transform.localPosition,targetPos[i],speed*Time.deltaTime);
+            }
+        }
+    }
+
+
 }
